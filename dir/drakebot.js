09bab6b00ec548f87c0bf7510998bab3574a4c6a
Michael DeMarco <michaelfromyeg@gmail.com>
2019-02-01 19:09:33 -0700

    Remove node attempt

--- dir/drakebot.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-var PythonShell = require('python-shell');
-
-var run = schedule.scheduleJob('00 * * * *', PythonShell.run('twitterbot.py', function (err) {
-  console.log('finished');
-}));
